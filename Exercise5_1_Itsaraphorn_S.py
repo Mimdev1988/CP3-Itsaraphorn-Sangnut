877b980334d51185430b95dce35e1ad49aea49ba
Mimdev1988 <163395888+Mimdev1988@users.noreply.github.com>
2024-05-02 09:15:55 +0700

    Exercise5_1_Itsaraphorn_S.py
     Exercise5_1_Itsaraphorn_S.py

---
+++ Exercise5_1_Itsaraphorn_S.py
@@ -0,0 +1,12 @@
+x = int(input("ป้อนค่า x: "))
+y = int(input("ป้อนค่า y: "))
+
+result1 = x + y
+result2 = x - y
+result3 = x * y
+result4 = x / y
+
+print("ผลบวกของ", x, "+", y, "=", result1)
+print("ผลลบของ", x, "-", y, "=", result2)
+print("ผลคูณของ", x, "*", y, "=", result3)
+print("ผลหารของ", x, "/", y, "=", result4)
